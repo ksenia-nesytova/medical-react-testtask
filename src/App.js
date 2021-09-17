238d43f415e002ff428733c73f3ba1347c75a6bf
ksenia nesytova <alleheldentot@gmail.com>
2021-09-17 14:50:36 +0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React from "react"
+import {Switch, Route, Link} from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Home from "./Components/Home"
+import Contacts from "./Components/Contacts"
+import Greeting from "./Components/Greeting"
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/contacts">Contacts</Link>
+        </nav>
+
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route  path="/greeting"><Greeting /></Route>
+          <Route  path="/contacts"><Contacts /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App
---
+++ src/Components/Greeting.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Greeting() {
+    return (
+        <h1>Привет</h1>
+    )
+}
+
+export default Greeting
