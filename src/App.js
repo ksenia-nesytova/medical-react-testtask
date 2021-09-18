5eb4b8cf3149498baf7ddbb63d54e26185533cdd
ksenia nesytova <alleheldentot@gmail.com>
2021-09-18 11:22:02 +0700

    added custom hook isLoggedIn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from "react"
+import React, {useState} from "react"
 import {Switch, Route, Link} from "react-router-dom"
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import Home from "./Components/Home"
 import Contacts from "./Components/Contacts"
-import Greeting from "./Components/Greeting"
+import Account from "./Components/Account"
 
 import './App.css';
 
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState({isLoggedIn: false})
+
+  function handleSubmit(event) {
+    setisLoggedIn(true)    
+  }
+
   return (
     <Router>
       <div>
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/contacts">Contacts</Link>
+          <Link to="/contacts">Контакты</Link>
         </nav>
 
         <Switch>
           <Route exact path="/"><Home /></Route>
-          <Route  path="/greeting"><Greeting /></Route>
+          <Route  path="/account"><Account /></Route>
           <Route  path="/contacts"><Contacts /></Route>
         </Switch>
       </div>
--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-function Greeting() {
+function Account() {
     return (
         <h1>Привет</h1>
     )
 }
 
-export default Greeting
+export default Account
